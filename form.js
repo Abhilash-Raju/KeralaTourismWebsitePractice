e9ae7fac8b20d42cc2614999f0d9c4012a6fa933
Abhilash Raju <abhilash.raju8@gmail.com>
2022-01-10 23:32:05 +0530

    Changes and JS practice
    Color changes and JS form validation practice

--- form.js
+++ form.js
@@ -1,32 +1,33 @@
 let email = document.getElementById("email");
 let pwd = document.getElementById("pwd");
-
-function validate(){
-    if (email.value==""||pwd.value=="")
-    {
-        alert("Fields can not be Empty");
-        return false;
-    }
-    else{
-           return true;
-    }
-}
-
 let email1 = document.getElementById("email1");
 let pwd1 = document.getElementById("pwd1");
 let pwd2 = document.getElementById("pwd2");
 
-function validate2(){
-    if (email1.value==""||pwd1.value==""||pwd2.value=="")
+function validate(){
+    if (email.value==""||pwd.value==""||email1.value==""||pwd1.value==""||pwd2.value=="")
     {
         alert("Fields can not be Empty");
         return false;
     }
+    
     else if(pwd1.value!=pwd2.value){
-        alert("Password does not match. Please re-enter the correct password.")
+        alert("Password does not match. Please re-enter the correct password.");
+        pwd2.style.border ="2px solid red";
            return false;
+
     }
-    else{
+    else if(pwd.value.length<=5 ){
+            alert("Password is too short");
+            pwd.style.border ="2px solid red";
+            return false;
+    }
+    else if((pwd1.value.length<=5) || (pwd2.value.length<=5) ){
+        alert("Password is too short");
+            pwd2.style.border ="2px solid red";
+            return false;
+    }
+    else {
         return true;
     }
 }
